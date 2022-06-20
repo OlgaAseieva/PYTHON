4b028414b992ae98aaee4cf82449a03dee5e9707
Aso <olga7791a@gmail.com>
2022-06-20 12:40:13 +0300

    task_1_pro_oder

--- pythonProject/Pythom_pro_2022_1/ht_1.0.py
+++ pythonProject/Pythom_pro_2022_1/ht_1.0.py
@@ -15,7 +15,10 @@ buyer_3 = Buyer('Musay', 'G', 'o678345')
 print(buyer_1, buyer_2,buyer_3, sep='\n')
 print('\n', dish_1, dish_2, dish_3, dish_4, sep='\n')
 
-oder_1 = Oder(dish_1, buyer_1, 2)
+oder_1 = Oder(buyer_1)
+oder_1.add_cart(dish_1,2)
+oder_1.add_cart(dish_2,1)
 
 
 print(oder_1)
+print (res_list)
--- pythonProject/Pythom_pro_2022_1/oder.py
+++ pythonProject/Pythom_pro_2022_1/oder.py
@@ -2,32 +2,44 @@ import datetime
 from buyer import Buyer
 from product import Product
 
+
 class Oder:
 
     """
     Class for added the cart for buyer and calculate the total amount
     """
-    def __init__(self, dish, client, q, *arg, **kwargs):
+    def __init__(self, client, *arg, **kwargs):
         self.customer = client
-        self.perchase = []
+        self.purchase = []
         self.quantity = []
-        self.dish = dish
 
-    def add_cart (self, *arg, **kwargs):
-        # if self.dish not in self.perchase:
-        #     self.purchase.append(Product)
-        #     self.quantity.append(q)
-        # else:
-        #     k = self.purchase.index (Product)
-        #     self.quantity[k] += 1
-        res_list = f'- {self.perchase[i]}  -- {self.quantity[i]}  = {self.quantity[i] * {Product.price}  '
+    def add_cart (self, dish, q, *arg, **kwargs):
+        if dish not in self.purchase:
+            self.purchase.append(dish)
+            self.quantity.append(q)
+        else:
+            k = self.purchase.index (dish)
+            self.quantity[k] += 1
+        print('oder:', '\n', ''.join(map(str, self.purchase)), ' --', ''.join(map(str, self.quantity)),'\n')
+        res_list = ''
+        for i in range len (self.quantity):
+            res_list += ''.join(map(str, self.purchase[i]))+ ' --'+ ''.join(map(str, self.quantity[i])) + '\n'
+
         return res_list
-    def info_client (self, client)
+
+    def info_client (self):
+
         return f'{self.customer}'
 
     def total_amount (self):
-        return sum([getattr(i, 'price') for i in self.perchase])
+        total = sum([getattr(i, 'price') * self.quantity[i] for i in self.purchase])
+        print(total)
+        return total
 
     def __str__(self):
-        return f'oder: {datetime.date.today()} \n {self.customer} \n {res_list}'
+        final = f'oder: {datetime.date.today()} \n '
+        final += ''.join(map(str, self.customer)) + '\n'
+        final += res_list+'\n'
+        final += f'Total amount: {str(total)} UAH'
+        return final
 
